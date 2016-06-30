b0779d4b977dbf7c76c178f35eaee533c43e856d
June <oleooo@126.com>
2016-06-30 17:42:27 +0800

    dp didn't fix

---
+++ Leetcode/25-JumpGame.py
@@ -0,0 +1,36 @@
+"""
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+"""
+
+
+class Solution(object):
+    def canJump(self, nums, index=0):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if index + 1 == len(nums):
+            return True
+        elif index >= len(nums):
+            return False
+        step = nums[index]
+        for i in range(1, step + 1)[::-1]:
+            if self.canJump(nums, index + i):
+                return True
+        return False
+
+
+s = Solution()
+print(s.canJump(
+    [2, 0, 6, 9, 8, 4, 5, 0, 8, 9, 1, 2, 9, 6, 8, 8, 0, 6, 3, 1, 2, 2, 1, 2, 6, 5, 3, 1, 2, 2, 6, 4, 2, 4, 3, 0, 0, 0,
+     3, 8, 2, 4, 0, 1, 2, 0, 1, 4, 6, 5, 8, 0, 7, 9, 3, 4, 6, 6, 5, 8, 9, 3, 4, 3, 7, 0, 4, 9, 0, 9, 8, 4, 3, 0, 7, 7,
+     1, 9, 1, 9, 4, 9, 0, 1, 9, 5, 7, 7, 1, 5, 8, 2, 8, 2, 6, 8, 2, 2, 7, 5, 1, 7, 9, 6]))
